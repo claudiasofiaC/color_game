c6152cd292dc7bc2f87d72120df5c402603b0a14
claudiasofiaC <claudiasofiaC@users.noreply.github.com>
2020-09-22 14:24:19 -0400

    adding game code

--- colors.py
+++ colors.py
@@ -22,4 +22,61 @@ def startGame(event):
 
 # function to choose and display the next color
 def nextColor():
-    # use the globally declared 'score'
+    # use the globally declared 'score' and 'play' variables
+    global score
+    global timeleft
+
+    # if game is currently in play
+    if timeleft > 0:
+
+        # make the text entry box active
+        e.focus_set()
+
+        # if the color typed is correct
+        if e.get().lower() == colors[1].lower():
+            score += 1
+
+        # clear the text entry box
+        e.delete(0, tkinter.END)
+
+        random.shuffle(colors)
+
+        # change the color to type, by changing the text and the color to a random color value
+        label.config(fg=str(colors[1]), text=str(colors[0]))
+
+        # update the score
+        scoreLabel.config(text='Score: ', str(score))
+
+# countdown timer function
+def countdown():
+    global timeleft
+
+    # if game is in play
+    if timeleft > 0:
+        # decrement the timer
+        timeleft -= 1
+
+        # update the time left label
+        timeLabel.after(1000, countdown)
+
+# driver code
+# create a GUI window
+root = tkinter.TK()
+
+# set the title
+root.title('Color Game')
+
+# set the size
+root.geometry('375x200')
+
+# add the instruction label
+instructions = tkinter.Label(root, text='Type the color of the words, and not the word text!',
+                             font=('Helvetica', 12))
+instructions.pack()
+
+# add the score label
+scoreLabel = tkinter.Label(root, text='Press enter to start',
+                           font=('Helvetica', 12))
+scoreLabel.pack()
+
+# add a time label
